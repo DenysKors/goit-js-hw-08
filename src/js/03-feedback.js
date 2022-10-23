bb9b79b899a4f71f8022966bdd0669274ac56a75
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-10-23 13:25:18 +0300

    FeedBack Data Done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,48 @@
 import throttle from 'lodash.throttle';
 
-const formRef = document.querySelector('.feedback-form')
-const inputRef = document.querySelector('input')
-const textAreaRef = document.querySelector('textarea')
+const refs = {
+    formRef: document.querySelector('.feedback-form'),
+    inputRef: document.querySelector('input'),
+    textAreaRef: document.querySelector('textarea'),
+}
 
-formRef.addEventListener('submit', onFormSubit);
-formRef.addEventListener('input', throttle(onFormInput, 500));
+refs.formRef.addEventListener('submit', onFormSubit);
+refs.formRef.addEventListener('input', throttle(onFormInput, 500));
 window.addEventListener('load', onWndwLoad);
-
-const userLocalData = {
-    
-};
 
 function onWndwLoad() { 
     if (localStorage.getItem('feedback-form-state') !== null) { 
         const userSavedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-        if (userSavedData.email) {
-            inputRef.value = userSavedData.email;
-            }
-        if (userSavedData.message) {
-            textAreaRef.value = userSavedData.message;
-        }
+
+        refs.inputRef.value = userSavedData.email;
+        refs.textAreaRef.value = userSavedData.message;
     }
 }
 
 function onFormInput(event) {
+    userLocalData = {
+        email: `${refs.inputRef.value}`,
+        message: `${refs.textAreaRef.value}`,
+    };
+
     if (event.target.name === 'email') {
         userLocalData.email = event.target.value;
-    }
+    } else
+        
     if (event.target.name === 'message') {
         userLocalData.message = event.target.value;
     }
+
  localStorage.setItem('feedback-form-state', JSON.stringify(userLocalData));
 }
 
 function onFormSubit(event) { 
     event.preventDefault();
+
     if (event.currentTarget.email.value === '' || event.currentTarget.message.value === '') {
-        return alert('Пожалуйста заполните поля!')
+        return alert('Пожалуйста заполните поля Email и Message!');
     }
+
     console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
     localStorage.removeItem('feedback-form-state');
     event.target.reset();
